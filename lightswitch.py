7a288781f3604e5a97cf68249e5f041d593bc99a
AkashRamlal1 <90188404+AkashRamlal1@users.noreply.github.com>
2022-02-09 09:29:09 +0100

    Add files via upload

--- lightswitch.py
+++ lightswitch.py
@@ -1,13 +1,42 @@
+
+from cgitb import text
 import tkinter as tk
+
+
 window = tk.Tk()
 
-button = tk.Button(text='...', bg="white", fg="black")
-button.pack(pady = 20, padx = 20)
+is_off = True
 
-# schijf hier tussen je code
+button = tk.Button(text = '')
+def  licht_aan_uit():
+    global is_off
+    
+    if is_off == True :
+        window.config(bg='yellow')
+        print('licht aan')
+        is_off = False
+
+    else:
+        window.config(bg="black")
+        print('licht uit')
+        is_off = True
+        
+    if is_off == False:
+        button.config(text = 'on')
+    elif is_off == True:
+        button.config(text = 'off')
+
+
+button = tk.Button(text='klik op mij ', command=licht_aan_uit)
+
+    
+button.pack()
 
 
 
-# schijf hier tussen je code
+
+# schijf hier tussen
+
+# schijf hier tussen je code 
 
 window.mainloop()
